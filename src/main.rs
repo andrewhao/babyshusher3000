dbb66661b338c0b9cd8a8a4eba94f47e706f2f23
Andrew Hao <andrewhao@gmail.com>
2022-02-05 07:23:40 -0800

    Adding clap command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
+use clap::Parser;
 use include_dir::{include_dir, Dir};
 use rand::prelude::*;
 use rand_distr::Normal;
 use soloud::*;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Duration in ms we should delay between shushes
+    #[clap(short, long, default_value_t = 1000)]
+    delay_ms: u64,
+
+    /// Jitter factor to add to delays (as standard deviations on normal distribution)
+    #[clap(short, long, default_value_t = 200)]
+    jitter: u64,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const JITTER_MEAN: f64 = 1000.0;
+    let args = Args::parse();
+
     let sl = Soloud::default()?;
     let mut wav = audio::Wav::default();
 
     static PROJECT_DIR: Dir<'_> = include_dir!("audio");
     let shh_mp3 = PROJECT_DIR.get_file("shh.mp3").unwrap();
 
-    // wav.load(&std::path::Path::new("shh.mp3"))?;
     wav.load_mem(&shh_mp3.contents())?;
 
     loop {
         println!("Shushing...");
         // let jitter_ms = thread_rng().gen_range(1..JITTER_MAX);
-        let poi = Normal::new(JITTER_MEAN, 200.0).unwrap();
-        let jitter = poi.sample(&mut thread_rng()) as u64;
+        let poi = Normal::new(args.delay_ms as f64, args.jitter as f64).unwrap();
+        let jitter_ms = poi.sample(&mut thread_rng()) as u64;
 
         sl.play(&wav); // calls to play are non-blocking, so we put the thread to sleep
         while sl.voice_count() > 0 {
             std::thread::sleep(std::time::Duration::from_millis(100));
         }
-        println!("Jitter: {}", jitter);
-        std::thread::sleep(std::time::Duration::from_millis(jitter));
+        println!("Jitter: {}", jitter_ms);
+        std::thread::sleep(std::time::Duration::from_millis(jitter_ms));
     }
 }
